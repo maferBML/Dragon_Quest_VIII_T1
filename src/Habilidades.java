66d9fb92528c24059fef0db550df3695087e8c94
Oliverdjab <oliver.arboleda@correounivalle.edu.co>
2025-10-05 10:57:04 -0500

    Implemente rama habilidades

---
+++ src/Habilidades.java
@@ -0,0 +1,15 @@
+public class Habilidades {
+    private String[] habilidades;
+
+    public Habilidades(String[] habilidades) {
+        this.habilidades = habilidades;
+    }
+
+    public String[] getHabilidades() {
+        return habilidades;
+    }
+
+    public void setHabilidades(String[] habilidades) {
+        this.habilidades = habilidades;
+    }
+}
